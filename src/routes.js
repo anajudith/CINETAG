e9d9dc055e072b961ff47bda93472ec9b8a0dae7
Ana Judith <ana-judth@hotmail.com>
2023-01-06 18:59:38 -0300

    lista dinamica, conectada com o db.json

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,19 @@
 import Inicio from "./pages/Inicio";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Favoritos from "./pages/Favoritos";
+import Cabecalho from "./components/Cabecalho";
+import Rodape from "./components/Rodape";
 function AppRoutes() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Inicio />}></Route>
-            </Routes>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Cabecalho />
+      <Routes>
+        <Route path="/" element={<Inicio />}></Route>
+        <Route path="/favoritos" element={<Favoritos />}></Route>
+      </Routes>
+      <Rodape />
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
